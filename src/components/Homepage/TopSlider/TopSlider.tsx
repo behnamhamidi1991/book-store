eaf19f930063e5b8c8dc05b7d24d922763fccd25
behnamhamidi1991 <eslp91@hotmail.com>
2024-02-13 07:47:29 +0330

    Commit

---
+++ src/components/Homepage/TopSlider/TopSlider.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { EffectFade, Navigation, Pagination } from "swiper/modules";
+import Image from "next/image";
+import "./topslider.css";
+
+import Image1 from "../../../../public/images/1.jpg";
+
+const TopSlider = () => {
+  return (
+    <>
+      <Swiper
+        spaceBetween={30}
+        effect={"fade"}
+        navigation={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[EffectFade, Navigation, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <Image src={Image1} className="image" />
+        </SwiperSlide>
+        <SwiperSlide></SwiperSlide>
+        <SwiperSlide></SwiperSlide>
+      </Swiper>
+    </>
+  );
+};
+
+export default TopSlider;
