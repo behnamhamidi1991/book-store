fc2fa30c1965c0a581d66d01d3c463feda5e27d0
Behnam Hamidi <eslp91@hotmail.com>
2024-03-10 20:42:42 +0330

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
 "use client";
 import Homepage from "@/components/Homepage/Homepage";
-import Image from "next/image";
-import { useState } from "react";
-
 export default function Home() {
   return (
     <main className="book-store">
